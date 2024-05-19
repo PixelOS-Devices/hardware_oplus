6d91c5682fdb48dd15899d9a25644918115b81a1
pjgowtham <pjgowtham@gmail.com>
2024-05-19 15:24:19 +0530

    hidl: fingerprint: Update fingerprint shim to operate with property_get
    Change-Id: I913f00db1542a28aaaa11dba93fcce67b39717bf

--- hidl/fingerprint/BiometricsFingerprintShim.cpp
+++ hidl/fingerprint/BiometricsFingerprintShim.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,9 @@
 #include <dlfcn.h>
 #include <string.h>
 
-extern "C" int __system_property_get(const char* __name, char* __value) {
+extern "C" {
+
+int __system_property_get(const char* __name, char* __value) {
     static auto __system_property_get_orig = reinterpret_cast<typeof(__system_property_get)*>(
             dlsym(RTLD_NEXT, "__system_property_get"));
 
@@ -37,3 +39,28 @@ extern "C" int __system_property_get(const char* __name, char* __value) {
 
     return __system_property_get_orig(__name, __value);
 }
+
+int property_get(const char* key, char* value, const char* default_value) {
+    static auto property_get_orig =
+            reinterpret_cast<typeof(property_get)*>(dlsym(RTLD_NEXT, "property_get"));
+
+    // Do we really need this override ?
+    if (strcmp(key, "ro.boot.mode") == 0) {
+        ALOGV("Returning normal for ro.boot.mode");
+        return strlen(strcpy(value, "normal"));
+    }
+
+    if (strcmp(key, "ro.boot.vbmeta.device_state") == 0) {
+        ALOGV("Returning unlocked for ro.boot.vbmeta.device_state");
+        return strlen(strcpy(value, "unlocked"));
+    }
+
+    // This property seems unused in the newer fingerprint implementation
+    if (strcmp(key, "ro.boot.verifiedbootstate") == 0) {
+        ALOGV("Returning orange for ro.boot.verifiedbootstate");
+        return strlen(strcpy(value, "orange"));
+    }
+
+    return property_get_orig(key, value, default_value);
+}
+} // extern "C"
